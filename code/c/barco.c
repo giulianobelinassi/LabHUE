ec3bca2429d9df3916c4e862b50710cfd799bd7b
Thais Lima de Sousa <thaislisousa@gmail.com>
2015-05-06 21:00:17 -0300

    insere barco alterada

--- code/c/barco.c
+++ code/c/barco.c
@@ -22,13 +22,32 @@ int rema_barco(Mapa* mapa, Barco* barco)
 	return 0;
 }
 
-int posiciona_barco(Mapa* mapa, Barco* barco)
+/** Sucesso em posicionar o barco retorna 0
+ */
+int posiciona_barco(pMapa mapa, pBarco barco)
 {
+	int pos, valido;
+
 	#ifdef DEBUG
 	static char funcname[] = "Chamado: posiciona_barco(%p, %p)\n";
 	debug_message(funcname, mapa, barco);
 	#endif
 	
+	printf("Escolha uma coluna para posicionar o barco. ");
+	printf("Entre com uma coluna valida: entre 0 e %d\n", mapa -> largura);
+	scanf("%d", &pos);
+	if(pos <= 0 && pos >= mapa -> largura)
+		valido = 0;
+	while(!valido && mapa -> matriz[0][pos] != '.')
+	{
+		printf("Posicao inválida! Tente novamente\n");
+		scanf("%d", &pos);
+		if(pos >= 0 && pos <= 9) valido = 1;
+	}
+	mapa -> matriz[0][pos] = 'B';
+	barco->linha = 0;
+	barco->coluna = pos;
+
 	#ifdef DEBUG
 	debug_message("Prestes a retornar 0\n");
 	#endif
